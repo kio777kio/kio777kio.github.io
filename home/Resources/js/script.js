e16436c432a4b9c647e3e4d2bf42b5ad5e5c0201
kio777kio <75054702+kio777kio@users.noreply.github.com>
2020-12-17 15:56:42 +0900

    adding a landing page

---
+++ home/Resources/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+        $('h1').click(function(){
+        $(this).css('background-color', '#ffffff')
+        })
+
+        $('.logo-plumeria').hide(0).delay(500).fadeIn(2500)
+
+        $('.logo-links').hide(0).delay(1500).fadeIn(3000)
+
+        $('.fukugumi-title h1').hide(0).delay(300).fadeIn(2500)
+
+
+});
